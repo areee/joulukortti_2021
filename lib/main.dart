d27e555958a268e66279277b8ec0717a29be83e5
areee <10089872+areee@users.noreply.github.com>
2021-12-24 10:19:14 +0200

    Update main.dart (first working animation BUT still without text)

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Joulukortti 2021',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
+      home: MyRiveAnimation(),
+    ));
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyRiveAnimation extends StatelessWidget {
+  const MyRiveAnimation({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Hyvää joulua ja onnellista uutta vuotta 2022!',
-            ),
-            RiveAnimation.asset('assets/joulukortti-animaatio.riv'),
-          ],
+        child: RiveAnimation.asset(
+          'assets/joulukortti-animaatio.riv',
+          fit: BoxFit.cover,
         ),
       ),
     );
