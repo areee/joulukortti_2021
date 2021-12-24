5ea0d70d9f759f0c262fe2ebca871bcda4766660
areee <10089872+areee@users.noreply.github.com>
2021-12-24 11:03:40 +0200

    Update .riv file + make a first working version with a greeting text (incl. Google Fonts)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -11,12 +12,29 @@ class MyRiveAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: RiveAnimation.asset(
-          'assets/joulukortti-animaatio.riv',
-          fit: BoxFit.cover,
-        ),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          const RiveAnimation.asset(
+            'assets/joulukortti_2021.riv',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: SizedBox(
+              width: 500,
+              child: Text(
+                'Hyvää joulua ja onnellista uutta vuotta 2022!',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.pacifico(
+                    textStyle: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                )),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
